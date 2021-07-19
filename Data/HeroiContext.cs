8c02c54ef1df06fde2141ae484cc33d6ced49666
Gustavo Morilla <gu_morilla@hotmail.com>
2021-07-19 13:11:52 -0300

    Adicionar arquivos de projeto.

---
+++ Data/HeroiContext.cs
@@ -0,0 +1,44 @@
+﻿using EFCore.WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    // Scaffold - precisamos passar alguns parãmentros durante a criação de um projeto EFCore DB First
+    // scaffold-dbcontext -provider microsoft.entityframeworkcore.sqlserver -connection ""
+
+    // para rodarmos esse comando em uma consoleapp precisaremos rodar  install-package entityframeworkcore.design
+
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<identidadeSecreta> IdentidadesSecretas { get; set; }
+
+        // o contexto é responsável por criar as tabelas no nosso banco de dados quando temos classes e utilizamos code-first
+        // As propriedades precisam estar no plural por ser um elemento List que contém N elementos.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=HeroApp;Data Source=DESKTOP-FKC65HE");
+            // Catalog é o nome do banco de dados
+        }
+
+        /* Tivemos um erro na criação da tabela de HeroiBatalha, o contexto não identifica sozinho que se trata de uma chave composta e cobra a
+        especificação de uma chave primária, então faremos o override abaixo. */
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity => 
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+                // usamos uma lambda expression declarando um objeto anônimo e em seguida especificamos a nossa chave composta
+            });
+        }
+    }
+}
---
+++ Model/Heroi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Model
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public identidadeSecreta Identidade { get; set; }
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
+
+/* Quando iniciamos um projeto EFCore com CodeFirst o passo inicial é identificar e configurar os relacionamentos, por padrão sempre
+ * que temos um relacionamento - um para muitos - como no caso de Heroi que pode ter muitas armas, declaramos uma propriedade do tipo
+ * List<>/ IEnumerable/ ICollection dessa classe, para conseguirmos listar todas as armas que são daquele heroi. Na entidade armas apenas
+ * referenciamos a entidade Heroi e uma prop int para o ID. */
+
+// Em - um para um - neste caso nem sempre um heroi tem que necessariamente ter uma identidade secreta, por isso não declaramos uma prop Id para heroi.
+
+/* No caso de - muitos para muitos - temos a possibilidade de criar uma nova entidade intermediária que irá conter todas as propriedades necessárias. */
